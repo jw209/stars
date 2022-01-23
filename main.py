ed55fcfb3cff8afd9680344420161b40b919ca57
jw209 <jw209@uakron.edu>
2022-01-23 16:48:53 -0500

    Add matplotlib and create some figures

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier
+from matplotlib import cm
+from pandas.plotting import scatter_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 
@@ -24,3 +27,30 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_
 knn.fit(X_train, y_train)
 
 print(knn.score(X_test, y_test))
+
+k_range = range(1, 20)
+scores = []
+
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    scores.append(knn.score(X_test, y_test))
+
+cmap = cm.get_cmap('gnuplot')
+scatter = scatter_matrix(X, c=y, marker='o', s=40, hist_kwds={'bins': 15}, figsize=(9, 9), cmap=cmap)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X['Temperature'], X['L'], X['R'], c=y, marker='o', s=100)
+
+ax.set_xlabel('Temperature')
+ax.set_ylabel('L')
+ax.set_zlabel('R')
+
+plt.figure()
+plt.xlabel('k')
+plt.ylabel('accuracy')
+plt.scatter(k_range, scores)
+plt.xticks([0, 5, 10, 15, 20])
+
+plt.show()
