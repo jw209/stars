33893570e163747594176fa5b22c253335d889b8
Jesse <77357219+jw209@users.noreply.github.com>
2022-01-30 18:44:17 -0500

    Add code to identify unknown real-world star

--- main.py
+++ main.py
@@ -44,13 +44,13 @@ stars['Spectral_class_label'] = le.transform(stars.Spectral_Class)
 X = stars[['Temperature', 'L', 'R', 'A_M', 'Color_label', 'Spectral_class_label']]
 y = stars['Type']
 
-knn = KNeighborsClassifier(n_neighbors=3, metric='euclidean')
+knn = KNeighborsClassifier(n_neighbors=3, metric='manhattan')
 
 knn.fit(X, y)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.25, random_state=42)
 
-print("kNN using 'Euclidean Distance'")
+print("kNN using 'Manhattan Distance'")
 knn.fit(X_train, y_train)
 score = knn.score(X_test, y_test)
 print("Accuracy :", "{:.0%}".format(round(score, 2)))
@@ -81,7 +81,7 @@ print(cm)
 # create and show temperature and color bar plot
 fig, ax = plt.subplots()
 stars.groupby('Color')['Temperature'].mean().plot.bar()
-plt.gcf().subplots_adjust(bottom=0.25, left=0.22)
+plt.gcf().subplots_adjust(bottom=0.40, left=0.22)
 plt.ylabel('Average Temperature (K)')
 
 # create temperature and star type bar plot
@@ -127,5 +127,18 @@ print('\nDescribing white dwarf stars: \n', white_dwarf.describe())
 print('\nDescribing main sequence stars: \n', main_sequence.describe())
 print('\nDescribing super giant stars: \n', super_giants.describe())
 print('\nDescribing hyper giant stars: \n', hyper_giants.describe())
+
+# testing real world star (we are trying to see if Betelgeuse is a super giant)
+# spectral type: M1-M2, A_M: -5.85, Temperature: 3600+-200, L: 126,000, R: 764
+
+unknown = pd.DataFrame([[3600, 126000, 764, -5.85, 'Red', 'M']], columns=['Temperature', 'L', 'R', 'A_M', 'Color', 'Spectral_Class'])
+le.fit(unknown.Color)
+unknown['Color'] = le.transform(unknown.Color)
+le.fit(unknown.Spectral_Class)
+unknown['Spectral_Class'] = le.transform(unknown.Spectral_Class)
+
+star_prediction = knn.predict(unknown)
+print('The unknown star you are predicting is apart of: ', Type_label[star_prediction[0]], ' star type')
+print(knn.predict_proba(unknown))
 
 plt.show()
